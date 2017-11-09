6be904117b74c726030cae847df81c83428c266d
ENTI\hzhou <huiyuan_97@hotmail.com>
2017-11-09 19:53:33 +0100

    Animation works

--- Super_Mario_Bros/js/Levels/Level1/level1.js
+++ Super_Mario_Bros/js/Levels/Level1/level1.js
@@ -5,9 +5,8 @@ var marioBros = marioBros || {};
 marioBros.level1 = {
     preload:function(){
      this.game.physics.startSystem(Phaser.Physics.ARCADE);
-     this.load.spritesheet('mario','img/Player/Mario_Small/mario_small_idle.png',13,16);
-     this.load.spritesheet('running','img/Player/Mario_Small/mario small run.png',36,16);
-
+     this.load.spritesheet('mario','img/Player/Mario Small/mario small idle.png',13,16);
+     this.load.spritesheet('runLeft','img/Player/Mario Small/mario small run.png',36,16);
     
     },
    
@@ -16,8 +15,9 @@ marioBros.level1 = {
         this.player = new marioBros.marioPrefab(this.game,150,150);
         this.player.anchor.setTo(0.5);
         this.player.scale.setTo(2);
-        this.player.animations.add('run',[0,1,2],true);
         
+        this.player.animations.add('runLeft',[0,1,2,3,4],6,false);
+        this.player.animations.add('mario',[0],0,false);
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.game.physics.arcade.enable(this.player);
@@ -26,17 +26,19 @@ marioBros.level1 = {
     
     update:function(){
         this.player.body.velocity.x = 0;
-        
+   
         if(this.cursors.left.isDown){
             this.player.body.velocity.x = -gameOptions.playerSpeed;
-            this.player.animations.play('run');
+            this.player.animations.play('runLeft');
             
             this.player.scale.x = -2;
          } else if (this.cursors.right.isDown){
             this.player.body.velocity.x = gameOptions.playerSpeed;
-            this.player.animations.play('run');
+            this.player.animations.play('runLeft');
           this.player.scale.setTo(2,2);            
-        } 
+        }else{
+                this.player.animations.play('mario');
+        }
     
     }
 };
--- Super_Mario_Bros/js/Player/player.js
+++ Super_Mario_Bros/js/Player/player.js
@@ -3,15 +3,17 @@ var marioBros = marioBros ||{};
 marioBros.marioPrefab = function(game,x,y)
 {
     Phaser.Sprite.call(this,game,x,y,'mario');
+    Phaser.Sprite.call(this,game,x,y,'runLeft');
+    this.animations.add('runLeft',[1,2,3],10,true);
     game.add.existing(this);
-    this.state = 'straight';
+    this.state = 'mario';
    
   //  game.physics.arcade.enable(this);
     this.anchor.setTo(0.5);
     this.scale.setTo(2);
    // this.checkWorldBounds = true;
  
-
+//oalaoaolaoao
         
 };
 marioBros.marioPrefab.prototype = Object.create(Phaser.Sprite.prototype);
