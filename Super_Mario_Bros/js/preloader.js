2eada7e6859fccd10448b3aba7533a09e51f7355
Marc Molina Torres <marc.molina.torres@gmail.com>
2017-11-27 18:27:25 +0100

    mario big spritesheet ok

--- Super_Mario_Bros/js/preloader.js
+++ Super_Mario_Bros/js/preloader.js
@@ -14,7 +14,7 @@ marioBros.preloader.prototype = {
         this.load.image('cursor', 'img/UI/Menu/cursor.png');
         this.load.tilemap('level1','tilemaps/level1.json',null,Phaser.Tilemap.TILED_JSON);
         this.load.image('tileset_levels','tilemaps/tileset_levels.png');
-        this.load.spritesheet('marioSmall', 'img/Player/Mario Small/Other_version/small_mario.png', 16, 16);
+        this.load.spritesheet('marioSmall', 'img/Player/Mario Small/small_mario.png', 16, 16);
         this.load.spritesheet('brick', 'img/Levels/brick.png', 16, 16);
         this.load.spritesheet('brickCoin', 'img/Levels/questionMarkBlock.png', 16, 16);
         this.load.spritesheet('brickCoins', 'img/Levels/brickCoins.png', 16, 16);
